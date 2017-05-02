46cd2c75ef9ed16262ec5b9a44f7d2b4e52c4f14
spica8080 <spica8080@gmail.com>
2017-05-02 11:45:41 -0500

    blog post rendered to the DOM

--- app/scripts/views/app_root.js
+++ app/scripts/views/app_root.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
+import BlogPost from "./blog_post";
 
 export default React.createClass({
   render: function() {
     return (
       <main>
         <NavBar />
+        <BlogPost />
 
       </main>
     );
---
+++ app/scripts/views/blog_post.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default React.createClass({
+  render: function() {
+    return (
+      <article className="blog_post">
+        <h2>The Mind</h2>
+        <p>
+          This #mind is not about drawing #conclusions - this is a tool for exploration.
+        </p>
+      </article>
+    );
+  }
+});
